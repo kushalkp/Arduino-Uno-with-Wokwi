61e3948135b86960ad01e3d10d6092f1687d9647
kushalkp <46906772+kushalkp@users.noreply.github.com>
2025-09-27 19:57:42 -0500

    Add files via upload

---
+++ Serial Monitor/Program 2/sketch.ino
@@ -0,0 +1,14 @@
+int counter = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.print("Counter value: ");
+  Serial.println(counter); // Print value and move to next line
+  counter++; // Increase counter by 1
+  delay(1000); // Wait for 1 second before repeating
+}
